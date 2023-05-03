29b980b5d00da1c8fefcde3bac02353861797fdb
Guillaume Bouly <gbouly1@gmail.com>
2023-05-03 13:50:43 +0200

    premier commit p9

---
+++ foce-child/oscar.php
@@ -0,0 +1,4 @@
+<div class="oscar-section">
+    <h3 class="oscar-text">Fleurs d`oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
+    <img src="<?php echo get_stylesheet_directory_uri();?>'/assets/images/logo_oscar_1.png'" alt="Logo Oscar">
+</div>

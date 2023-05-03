f055775d6d34d78c0277f392b8b15d336eac4890
Guillaume Bouly <gbouly1@gmail.com>
2023-05-04 00:05:16 +0200

    flottement du titre, animation au scroll, intégration de la vidéo dans le Hero banner

--- foce-child/js/index.js
+++ foce-child/js/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+// création de l'observateur
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("title-animation");
+    }
+  });
+});
+
+// on dit a l'obs quel élément tracker
+observer.observe(document.querySelector(".scroll-title"));
